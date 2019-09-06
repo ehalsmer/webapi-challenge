5ab81a0b5552ba3613e3080f79c0ac8e3a61dad6
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-06 11:14:44 -0600

    Can now get actions for a specific project, and post a new action to a project

---
+++ actions/actionsRouter.js
@@ -0,0 +1,5 @@
+// get / get all actions
+
+// get /:id get action by action id
+
+// put /:id edit action by id
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -14,6 +14,7 @@ const router = express.Router();
 router.use(express.json());
 
 const pModel = require("../data/helpers/projectModel");
+const aModel = require("../data/helpers/actionModel");
 
 //// middleware:
 
@@ -46,9 +47,35 @@ function validateProject(req, res, next) {
     res.status(400).json({ message: "missing required name field" });
   } else if (!req.body.description) {
     res.status(400).json({ message: "missing required description field" });
-  } else if (typeof req.body.description != 'string' || typeof req.body.name != 'string') {
-    res.status(400).json({error: "description and name must be of type string"})
+  } else if (
+    typeof req.body.description != "string" ||
+    typeof req.body.name != "string"
+  ) {
+    res
+      .status(400)
+      .json({ message: "description and name must be of type string" });
   }
+  next();
+}
+
+function validateAction(req, res, next) {
+  if (Object.keys(req.body).length == 0) {
+    res.status(400).json({ message: "missing required action data" });
+  } else if (!req.body.description) {
+    res.status(400).json({ message: "missing required description field" });
+  } else if (req.body.description.length >= 128) {
+    res.status(400).json({ message: "description too long - enter up to 128 characters"})
+  } else if (!req.body.notes) {
+    res.status(400).json({ message: "missing required notes field" });
+  } else if (
+    typeof req.body.description != "string" ||
+    typeof req.body.notes != "string"
+  ) {
+    res
+      .status(400)
+      .json({ message: "description and notes must be of type string" });
+  }
+  req.action = {...req.body, project_id: req.params.id}
   next();
 }
 
@@ -62,7 +89,7 @@ router.get("/", (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res.status(500).json({ error: "Error getting projects" });
+      res.status(500).json({ message: "error getting projects" });
     });
 });
 
@@ -73,13 +100,57 @@ router.get("/:id", validateId, (req, res) => {
 
 // post a new project
 router.post("/", validateProject, (req, res) => {
-  pModel.insert(req.body)
+  pModel
+    .insert(req.body)
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "error adding project to database" });
+    });
+});
+
+// edit a project
+router.put("/:id", validateId, validateProject, (req, res) => {
+  pModel
+    .update(req.params.id, req.body)
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "error updating project" });
+    });
+});
+
+// remove a project
+router.delete("/:id", validateId, (req, res) => {
+  pModel
+    .remove(req.params.id)
+    .then(response => {
+      res.status(200).json({ message: "project deleted" });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "error removing project" });
+    });
+});
+
+// get an array of a project's actions
+router.get("/:id/actions", validateId, (req, res) => {
+  res.status(200).json(req.project.actions);
+});
+
+// post a new action to a project
+router.post("/:id/actions", validateId, validateAction, (req, res) => {
+  aModel.insert(req.action)
   .then(response => {
       res.status(201).json(response)
   })
   .catch(error => {
       console.log(error)
-      res.status(500).json({error: 'error adding project to database'})
+      res.status(500).json({message: "error adding action to database"})
   })
 });
 
